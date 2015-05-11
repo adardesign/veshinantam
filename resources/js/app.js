a6b6a6a2f46e766b5348c15e7fd96094ee4751f9
Eliazer - home <adardesign@gmail.com>
2015-05-11 08:40:34 -0400

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+ $('#calendar').fullCalendar({
+   header: {
+     left: 'prev,next today',
+     center: 'title',
+     right: 'month,basicWeek,basicDay'
+   },
+   defaultDate: '2015-02-12',
+   editable: true,
+   eventLimit: true, // allow "more" link when too many events
+   eventSources: [
+
+     // your event source
+     {
+       url: "/resources/data/ibuhamNehaga.json", // use the `url` property
+       color: 'yellow', // an option!
+       textColor: 'black' // an option!
+     }, {
+       url: "http://www.hebcal.com/hebcal/?cfg=fc&v=1&i=off&maj=on&min=on&nx=on&mf=on&ss=on&mod=on&lg=s&s=on",
+       textColor: 'black' // an option!
+     }
+   ]
+ });
